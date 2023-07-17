384770e58522ba1fdd44800fcdcb8082d290eb4e
JAMOLJON <saidjamolpubg@gmail.com>
2023-07-17 18:27:09 +0500

    qurulma_haqida

---
+++ js/app.js
@@ -0,0 +1,53 @@
+let text = document.querySelector("h1")
+let text_hour = document.querySelector(".text");
+let text_platform = document.querySelector(".text-platform");
+let text_screen = document.querySelector(".text-screen");
+let text_ram = document.querySelector(".text-ram");
+let text_proccecor = document.querySelector(".text-proccecor");
+let text_location = document.querySelector(".text-location");
+let battery_lvl = document.querySelector(".text-battery");
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", ()=> {
+    
+    text.classList.add("open-text")
+
+    setInterval(()=> {
+        
+        const date = new Date()
+        let hours = date.getHours()
+        let minutes =  date.getMinutes()
+        let seconds = date.getSeconds()
+        
+        text_hour.innerHTML = "Soatingiz: " + hours + ":" + minutes + ":" + seconds
+        
+    }, 1000);
+    
+    let prosesorPatok = navigator.hardwareConcurrency;
+    
+    text_platform.innerHTML = `Operatsion tizimingiz: ${window.navigator.userAgentData.platform}`
+    text_screen.innerHTML = "Ekraningiz Hajimi: " + window.screen.width + " x " + window.screen.height
+    text_ram.innerHTML = `Xotirangiz: ${window.navigator.deviceMemory} GB`
+    text_proccecor.innerHTML = `Mantiqiy prosesorlaringiz: ${prosesorPatok}`
+    
+    console.log(`Operatsion Tizimingiz  ${window.navigator.userAgentData.platform}`);
+    console.log("Ekraningiz Hajimi " + window.screen.width + " x " + window.screen.height);
+    console.log(`${window.navigator.deviceMemory} GB Xotirangiz`);
+    console.log(`${prosesorPatok} Prosesoringizni patoki`);
+    
+    navigator.getBattery().then(function(battery) {
+        const level = battery.level
+        const status = level * 100 + "%"
+        battery_lvl.innerHTML = `Batareykangizni quvvati: ${status}`
+        console.log(`Batareya quvvati ${status}`);
+    })
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position)=> {
+            let lat = position.coords.latitude
+            let lon = position.coords.longitude
+            
+            text_location.innerHTML = `Qayerda turushingiz: ${lat} ${lon}`
+            console.log(`${lat} ${lon} Qayerda turushingiz`);
+        });
+    };
+});
